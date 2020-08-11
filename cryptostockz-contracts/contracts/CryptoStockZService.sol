0179c7750cb46b005a8102dc763e00b821013b81
alexmirmao <alexmirmao@gmail.com>
2020-08-11 12:42:03 +0200

    desacoplados storage y tokens

--- cryptostockz-contracts/contracts/CryptoStockZService.sol
+++ cryptostockz-contracts/contracts/CryptoStockZService.sol
@@ -2,16 +2,17 @@
 // solium-disable linebreak-style
 pragma solidity >=0.4.21;
 
-// import "./ProductToken.sol";
+import "./ProductToken.sol";
 import "./StockZStorage.sol";
 import "./Ownable.sol";
 import "./Product.sol";
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol';
 
-contract CryptoStockZ is Ownable, ERC721 {
+contract CryptoStockZ is Ownable {
     
-    constructor() ERC721("Stock Z Products", "SZP") public{}
+    //constructor() ERC721("Stock Z Products", "SZP") public{}
     StockZStorage stockZStorage = new StockZStorage(msg.sender);  // Creamos el storage 1.0
+    ProductToken productToken = new ProductToken();
 
     event createProductEvent(address _owner, string _name, string _ean, string _sku, uint256 _numberTransactions, uint8 _level);
     event transferTokenEvent(address _from, address _to, address _idProduct);
@@ -26,19 +27,29 @@ contract CryptoStockZ is Ownable, ERC721 {
         stockZStorage = StockZStorage(address(_StockZStorageAddr));
     }
 
+     // ProductToken Storage
+    /**
+    @notice sets the address of the Token contract
+    @dev Storage has all products association with their owners
+    @param _productTokenAddr address of the Storage contract
+    */
+    function setProductToken(address _productTokenAddr) public onlyOwner {
+        productToken = ProductToken(address(_productTokenAddr));
+    }
+
     function createProduct(string memory _ean, string memory  _sku, string memory _name) public {
         Product product = new Product(_ean,_sku,_name);
         uint256 tokenId = stockZStorage.getProducts().length;
         stockZStorage.setProduct(product);
         stockZStorage.setPositionProduct(product.getAddress(), tokenId);
-        _mint(msg.sender, tokenId);
+        productToken.mint(msg.sender, tokenId);
         emit createProductEvent(msg.sender,_name,_ean,_sku,0,0);
     }
     
     function transferProduct(address _to, address _idProduct) public {
         emit transferTokenEvent(msg.sender, _to, _idProduct);
         uint256 tokenId = stockZStorage.getProductToken(_idProduct);
-        _transfer(msg.sender, _to, tokenId);
+        productToken.transferToken(msg.sender, _to, tokenId);
         stockZStorage.setProduct(stockZStorage.getProducts()[tokenId]);
     }
 
@@ -53,15 +64,15 @@ contract CryptoStockZ is Ownable, ERC721 {
     
     function getOwnerOfProduct(address _idProduct) public view returns(address){
         uint256 tokenId = stockZStorage.getProductToken(_idProduct);
-        return super.ownerOf(tokenId);
+        return productToken.getOwner(tokenId);
     }
-    function getStorageAddress() public returns(address) onlyOwner{
+    function getStorageAddress() public view onlyOwner returns(address) {
         return address(stockZStorage);
     }
-    function getOwnerOfProduct(address _idProduct) public view returns(address){
-        uint256 tokenId = stockZStorage.getProductToken(_idProduct);
-        return super().ownerOf();
+    function getProductTokenAddress() public view onlyOwner returns(address) {
+        return address(productToken);
     }
+   
 }
 
 
