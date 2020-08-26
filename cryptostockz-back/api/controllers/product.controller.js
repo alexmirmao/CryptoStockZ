5ca00a5a5116b6c1ec5cfb0d4a80f885ef54a8c3
ppolo <benitopolo97@gmail.com>
2020-08-26 11:15:35 +0200

    Cryptostockz.controller -> product.controller

--- cryptostockz-back/api/controllers/product.controller.js
+++ cryptostockz-back/api/controllers/product.controller.js
@@ -1,35 +1,102 @@
 const db = require("../models");
-const Product = db.product;
 
-const cryptostockzService = require("../services/cryptostockz.service");
+const Product = db.product;
+const BaseProduct = db.base_product;
+const User = db.user;
 
 
 /**
- * Ejemplo de creacion de un producto
+ * Insercion de un nuevo producto digital en la BD
  * 
- * En la practica, esto llama a createProduct de sercives.
+ * En la practica, esta funcion es llamada desde el front cuando
+ * se recibe el evento de que se ha creado el producto en la
+ * Blockchain.
  */
 exports.createProduct = (req, res) => {
-    var products = req.body;
-    Product.bulkCreate({
-        products
-    })
-    .then(() => {
-        /*cryptostockzService.createProduct().then(result => {
-            
-        })*/
-        return res.status(200).send({ message: "Product(s) Created" });
-    })
-    .catch(err => {
+    User.findOne({
+        where: {
+            id: req.userId
+        }
+    }).then(user => {
+        if (!user) {
+            return res.status(404).send({ message: "User Not Found." });
+        }
+        var digitalProduct = req.body;
+        // Buscamos que exista el producto base en nuestra base de datos
+        BaseProduct.findOne({
+            where: {
+                id: digitalProduct.baseProductId
+            }
+        }).then(base_product => {
+            // Comprueba que existe el producto base en nuestra base de datos
+            if (!base_product) {
+                return res.status(200).send({ message: "Base Product does not exist." });
+            }
+            // Buscamos que el owner_address sea igual al valor de la cuenta de metamask guardada.
+            User.findOne({
+                where: {
+                    metamaskAccount: digitalProduct.owner_address
+                }
+            }).then(metamask_account => {
+                // Comprueba que existe la cuenta en nuestra base de datos
+                if (!metamask_account) {
+                    return res.status(200).send({ message: "Metamask account not match." });
+                }
+                Product.create({
+                    address: digitalProduct.address,
+                    owner_address: digitalProduct.owner_address,
+                    level: digitalProduct.level,
+                    baseProductId: digitalProduct.baseProductId,
+                    userId: metamask_account.dataValues.id
+                }).then(product => {
+                    //product.setOwner(metamask_account);
+                    return res.status(200).send({ message: "Digital product created." });
+                });
+            });
+        });
+    }).catch(err => {
         res.status(500).send({ message: err.message });
     });
-
-};
+}
 
 /**
  * Ejemplo de actualizacion de un producto (revisar)
  */
-exports.updateProduct = (req, res) => {
+exports.updateProductWithForm = (req, res) => {
+    User.findOne({
+      where: {
+          id: req.userId
+      }
+    }).then(user => {
+        if (!user) {
+            return res.status(404).send({ message: "User Not Found." });
+        }
+  
+        var authorities = [];
+        user.getRoles().then(roles => {
+            roles.forEach(element => {
+                authorities.push(element.name);
+            });
+            // Comprobamos que sea manufacturer o seller la persona que modifica un producto digital
+            if (authorities.includes("manufacturer")) {
+              var digitalProduct = req.body;
+              console.log(digitalProduct);
+              Product.update(
+                digitalProduct
+              ,
+              { where: 
+                {
+                  id: digitalProduct.productId
+                }
+              })
+              return res.status(200).send({ message: "Digital product updated." });
+            } else {
+              return res.status(404).send({ message: "Need permissions to update." });
+            }
+        });
+    }).catch(err => {
+        res.status(500).send({ message: err.message });
+    });
 };
 
 exports.getAllProducts = (req, res) => {
