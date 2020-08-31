87b3582e2aea632b4f75277d4971c30e1f238523
cristinacastrom <cristina.castro@pues.es>
2020-08-31 16:19:31 +0200

    Actualizacion registro/buscar

--- cryptostockz-front/src/components/Buscar.js
+++ cryptostockz-front/src/components/Buscar.js
@@ -1,43 +1,83 @@
-import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./Login.css";
+import React, {Component} from "react";
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
+import Navbar from "./Navbar";
+import Image from 'react-image-resizer';
+import Logo from "../Logo.png";
 
-export default function Buscar() {
-  const [id, setId] = useState("");
-  const [username, setUsername] = useState("");
 
-  function validateForm() {
-    return id.length > 0 || username.length > 0;
+export default class Buscar extends Component{
+
+  constructor(props){
+    super(props);
+      this.state={
+        username:"",
+        publicKey:"",
+        errorMessage:"",
+        successMessage:""
+      }
+      this.reset=this.reset.bind(this);
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  setValue(e){
+    let id=e.target.id;
+    let value=e.target.value;
+    let username=e.target.username;
+    if(value===undefined || value==="")
+      return;
+    if(id==="username"){
+      username=value.toUpperCase();
+      this.setState({
+        username:username})
+    }
+    else{
+      this.setState({
+        publicKey:e.target.value})
+    }
   }
 
-  return (
-    <div className="Buscar">
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="id" bsSize="large">
-          <ControlLabel>Public key</ControlLabel>
-          <FormControl
-            autoFocus
-            type="id"
-            value={id}
-            onChange={e => setId(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="username" bsSize="large">
-          <ControlLabel>Nombre de usuario</ControlLabel>
-          <FormControl
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Buscar
-        </Button>
-      </form>
-    </div>
-  );
+  submit(e){
+    if(this.state.username===undefined||this.state.username===""
+        || this.state.publicKey===undefined || this.state.publicKey===""){
+      this.setState({
+        errorMessage:"Debe ingresar nombre de usuario o clave"})
+        }
+    else{
+      this.setState({
+        successMessage:"Bienvenido a CryptoStockZ"})
+    }
+  }
+  reset(){
+    let username=this.state.username;
+    let publicKey=this.state.publicKey;
+      this.setState({
+        username:"",
+        publicKey:"",
+      })
+  }
+  render(){
+    return(
+      <form>
+            <h3 className="mx-auto text-center">Buscar</h3>
+
+            <div className="form-group mx-auto text-center">
+                <label>Username :  </label>
+                <input placeholder="Insert username" type="text" id="username" value={this.state.username} onChange={(e)=>this.setValue(e)} />
+
+            </div>
+
+            <div className="form-group mx-auto text-center" autofocus>
+                <label>Public key :  </label>
+                <input placeholder="Public key" type="publicKey" id="publicKey" value={this.state.publicKey} onChange={(e)=>this.setValue(e)} />
+            </div>
+            <button block bsSize="large" type="reset" className="btn btn-primary btn-block mx-auto text-center" onClick={this.reset}>Reset</button>
+            <button block bsSize="large" type="submit" className="btn btn-primary btn-block" onClick={(e)=>this.submit(e)}>
+              <Link to="/" className="btn btn-primary btn-block mx-auto text-center">
+                Buscar
+              </Link>
+            </button>
+        </form>
+
+
+    )
+  }
 }
