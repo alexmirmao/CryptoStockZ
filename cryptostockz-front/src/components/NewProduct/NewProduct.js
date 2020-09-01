867f83006c0a2cbc3d24e1b6b3f4276d7576e977
Pedro Polo <benitopolo97@gmail.com>
2020-09-01 20:38:30 +0200

    Comprueba si todos los campos se rellenan

--- cryptostockz-front/src/components/NewProduct/NewProduct.js
+++ cryptostockz-front/src/components/NewProduct/NewProduct.js
@@ -10,36 +10,51 @@ class NewProduct extends React.Component {
   state = {
     name: "",
     ean: "",
-    sku: ""
+    sku: "",
+    manufacturer: "",
+    isManufacturer: true
+  }
+
+  checkData() {
+    if (this.state.isManufacturer) {
+      return (this.state.name != "" && this.state.ean != "" && this.state.sku != "");
+    } else {
+      return (this.state.name != "" && this.state.ean != "" && this.state.sku != "" && this.state.manufacturer != "");
+    }
   }
 
   registerNewProduct(event) {
-    var data = JSON.stringify(
-      [
-        {
-          "name": this.state.name,
-          "ean": this.state.ean,
-          "sku": this.state.sku
-        }
-      ]);
+    if (this.checkData) {
+      alert("Complete all fields");
+    } else {
+      var data = JSON.stringify(
+        [
+          {
+            "name": this.state.name,
+            "ean": this.state.ean,
+            "sku": this.state.sku,
+            "manufacturer": this.state.manufacturer
+          }
+        ]);
 
-    var config = {
-      method: 'post',
-      url: 'http://192.168.1.42:10010/base/product',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNTk4OTgwNDI5LCJleHAiOjE1OTkwNjY4Mjl9.aPE3idLGpEuUw1eYS_jTqAF0z0xUm0tuVAbPGsssEXI'
-      },
-      data: data
-    };
+      var config = {
+        method: 'post',
+        url: 'http://192.168.1.42:10010/base/product',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNTk4OTgwNDI5LCJleHAiOjE1OTkwNjY4Mjl9.aPE3idLGpEuUw1eYS_jTqAF0z0xUm0tuVAbPGsssEXI'
+        },
+        data: data
+      };
 
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
   }
 
   updateName(event) {
@@ -68,6 +83,13 @@ class NewProduct extends React.Component {
     console.log(event.target.value);
   }
 
+  updateManu(event) {
+    this.setState({
+      manufacturer: event.target.value
+    });
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="Newproduct">
@@ -83,10 +105,16 @@ class NewProduct extends React.Component {
           <Form.Label>SKU</Form.Label>
           <Form.Control type="input" placeholder="Product Sku" onChange={(e) => this.updateSku(e)} />
         </Form.Group>
-        <Form.Group controlId="formBasicManu">
-          <Form.Label>Manufacturer</Form.Label>
-          <Form.Control type="input" placeholder="Manufacturer" />
-        </Form.Group>
+        {this.state.isManufacturer ? null : (
+          <Form.Group controlId="selectManu">
+            <Form.Label>Manufacturer</Form.Label>
+            <Form.Control as="select" onChange={(e) => this.updateManu(e)}>
+              <option>Choose...</option>
+              <option>Adidas</option>
+              <option>Nike</option>
+            </Form.Control>
+          </Form.Group>
+        )}
         <Button variant="primary" type="submit" onClick={e => this.registerNewProduct(e)}>
           Register
         </Button>
