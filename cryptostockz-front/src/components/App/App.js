7b7016f9707948fd2b758b65002203f529f844c5
Pedro Polo <benitopolo97@gmail.com>
2020-09-01 13:09:40 +0200

    Search Component

--- cryptostockz-front/src/components/App/App.js
+++ cryptostockz-front/src/components/App/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import NavBar from '../NavBar/NavBar';
 import UserProfile from '../UserProfile/UserProfile';
 import Home from '../Home/Home';
 import ProductView from '../ProductView/ProductView';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from '../Login/Login';
 import SignUp from '../Login/SignUp'
+import Search from '../Search/Search';
 
 function App() {
   return (
@@ -19,10 +20,10 @@ function App() {
         <Switch>
           <Route path="/home" component={Home} />
           <Route path="/profile" component={UserProfile} />
-          <Route path='/search' />
           <Route path='/profile/product/:productId' component={ProductView}/>
           <Route path='/login' component={Login}/>
           <Route path='/signup' component={SignUp}/>
+          <Route path='/search' component={Search}/>
         </Switch>
       </BrowserRouter>
     </React.Fragment>
