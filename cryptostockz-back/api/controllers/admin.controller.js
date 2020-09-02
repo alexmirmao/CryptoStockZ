70d5d0328212cc7b05f75aa065dab099b6d0e168
ppolo <benitopolo97@gmail.com>
2020-09-02 13:18:50 +0200

    New AdminController

--- cryptostockz-back/api/controllers/admin.controller.js
+++ cryptostockz-back/api/controllers/admin.controller.js
@@ -1,27 +1,6 @@
 const { session } = require("../middleware")
 const cryptostockzService = require("../services/cryptostockz.service");
 
-exports.allAccess = (req, res) => {
-  console.log("yes");
-  res.status(200).send("Public Content.");
-};
-  
-exports.userBoard = (req, res) => {
-  res.status(200).send("User Content." + session.getUserId(req, res));
-};
-  
-exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
-};
-  
-exports.manufacturerBoard = (req, res) => {
-  res.status(200).send("Manufacturer Content.");
-};
-
-exports.sellerBoard = (req, res) => {
-  res.status(200).send("Seller Content.");
-};
-
 exports.setStorage = (req, res) => {
   cryptostockzService.setStorage(req.body.storageAddress, req.body.cryptostockzAddress).then( result => {
     return res.status(200).send(result);
--- cryptostockz-back/api/routes/index.js
+++ cryptostockz-back/api/routes/index.js
@@ -3,6 +3,6 @@ const { app } = require('../bootstraps/swagger.bootstrap')
 // routes
 require('../routes/auth.routes')(app);
 require('../routes/account.routes')(app);
-require('../routes/views.routes')(app);
+require('../routes/admin.routes')(app);
 require('../routes/product.routes')(app);
 require('../routes/base.product.routes')(app);
